5dc41d914ebe883d0eb6b62ec14a73b8aab7a89c
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-10-15 14:50:40 -0400

    new build

---
+++ public/precache-manifest.f6885e198e5c18cfced4997c6202945b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafbffd0bb06e31b8c44f3776cf44023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc663e18fdea8da2d59",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "03b9a24ff83cb6680d7e",
+    "url": "/static/css/main.584deae1.chunk.css"
+  },
+  {
+    "revision": "4bc663e18fdea8da2d59",
+    "url": "/static/js/2.2c86a399.chunk.js"
+  },
+  {
+    "revision": "03b9a24ff83cb6680d7e",
+    "url": "/static/js/main.d67d685d.chunk.js"
+  },
+  {
+    "revision": "af59a38053367321f681",
+    "url": "/static/js/runtime~main.b8729eba.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
